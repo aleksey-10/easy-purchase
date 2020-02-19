9fc8a291572d151a1b308601e592d4c2c0a76e8a
aleksey-10 <alexcare1337@gmail.com>
2020-02-19 18:56:25 +0200

    Updates

---
+++ precache-manifest.687634eefd649c805fb8db524125ebe8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349359d1263b3228f6b498d0c7bcadce",
+    "url": "/easy-purchase/index.html"
+  },
+  {
+    "revision": "ca62f50585e764d69189",
+    "url": "/easy-purchase/static/css/main.2336c264.chunk.css"
+  },
+  {
+    "revision": "e494197c1a5e1e2d46fb",
+    "url": "/easy-purchase/static/js/2.d1957bf8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/easy-purchase/static/js/2.d1957bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca62f50585e764d69189",
+    "url": "/easy-purchase/static/js/main.d4aa8c13.chunk.js"
+  },
+  {
+    "revision": "1c5891ee5739168e84c4",
+    "url": "/easy-purchase/static/js/runtime-main.de4ba06d.js"
+  },
+  {
+    "revision": "5a911c8bc6feb3124fa1b2f6bc89fa80",
+    "url": "/easy-purchase/static/media/auchan.5a911c8b.png"
+  },
+  {
+    "revision": "7931c6a4b909e5318becef04968b364c",
+    "url": "/easy-purchase/static/media/novus.7931c6a4.png"
+  },
+  {
+    "revision": "6402b4324036242bafa459461e16bd2a",
+    "url": "/easy-purchase/static/media/silpo.6402b432.png"
+  },
+  {
+    "revision": "bcee6b19a200d51c7ee37e8403225e7d",
+    "url": "/easy-purchase/static/media/velmart.bcee6b19.png"
+  }
+]);
