81274382f1996935cb7b86ed5b4831a553af8d93
aleksey-10 <alexcare1337@gmail.com>
2020-09-09 20:44:09 +0300

    versions and styles update

--- src/components/Content/Todo/TodoList/TodoListContainer.jsx
+++ src/components/Content/Todo/TodoList/TodoListContainer.jsx
@@ -4,18 +4,25 @@ import TodoList from './TodoList';
 import Preloader from './../../../common/Preloader';
 
 class TodoListContainer extends React.Component {
-    render() {
-        return (this.props.markets.fetching) ? <Preloader /> : <>{
-            !this.props.market.length ? <h1 className="display-4">No todos</h1>
-                : <TodoList market={this.props.market} marketName={this.props.marketName} />
-        }</>
+  render() {
+    if (this.props.markets.fetching) {
+      return <Preloader />;
     }
+
+    return (
+      <>
+      {!this.props.market.length
+        ? <h1 className="display-4">No todos</h1>
+        : <TodoList market={this.props.market} marketName={this.props.marketName} />}
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-    markets: state.markets,
-    market: state.markets[ownProps.marketName],
-    marketName: ownProps.marketName
+  markets: state.markets,
+  market: state.markets[ownProps.marketName],
+  marketName: ownProps.marketName
 })
 
 export default connect(mapStateToProps)(TodoListContainer);
